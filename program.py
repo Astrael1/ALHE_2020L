3ee6090c39e148bcf77c59da92d14c78279a8520
Szymon Małolepszy <szymon8999@wp.pl>
2020-05-02 21:14:18 +0200

    Full argparse

--- program.py
+++ program.py
@@ -4,5 +4,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("city_1", help='Starting city')
 parser.add_argument("city_2", help='Ending city')
-parser.add_argument("-qas")
+parser.add_argument("type", help='type of algorithm to use', default='qas', choices=['qas', 'das'])
+parser.add_argument("-o", dest="output_path", help="path to output file")
 args = parser.parse_args()
+
+print("Calculating path from {} to {} with algorithm {}. I will store it in {}"
+    .format(args.city_1, args.city_2, args.type, args.output_path))
